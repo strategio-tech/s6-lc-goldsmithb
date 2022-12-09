9c4674036dfbf812e37056a0412891c321f8af99
Bradley Goldsmith <bradleygoldsmith14@gmail.com>
2022-12-09 12:06:57 -0500

    feat: implement getRemainingAmountIn3Months()

--- src/main/java/tech/strategio/LoanCalculator.java
+++ src/main/java/tech/strategio/LoanCalculator.java
@@ -5,15 +5,20 @@ import java.util.Scanner;
 public class LoanCalculator {
 
     /**
-     * TODO: finish out this JavaDoc comment block.
-     * FIXME: WHAT DOES THIS METHOD DO?
+     * Calculate the debt owed on a loan after three months,
+     * paying 10% back each month.
      * 
-     * @param ???
-     * @return ???
+     * @param amount the initial amount owed
+     * @return remaining amount owed after three months of payment
      */
     static int getRemainingAmountIn3Months(int amount) {
-        // TODO: Rewrite this method
-        return -1;
+        // constant variable to hold amount of months to consider - always 3
+        final int months = 3;
+        for (int i = months; i > 0; i--) {
+            int amountPaid = amount / 10;
+            amount -= amountPaid;
+        }
+        return amount;
     }
 
     public static void main(String[] args) {
